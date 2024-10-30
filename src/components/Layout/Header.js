154ed7182af5fc4bc68a484f68c8de9dabe6cdd3
= <topvpntool@gmail.com>
2024-10-30 23:16:02 +1100

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
             <div className="flex">
                 <a href="" className="flex items-center">
                     <img src="./web-logo.jpg" className="h-12 w-12 mr-1" />
-                    <span className="mr-4 text-2xl font-bold">Seek</span>
+                    <span className="mr-4 text-2xl font-bold">Geek</span>
                 </a>
                 <div className="flex items-center space-x-6">
                     <a href="" className="border-b-2 border-black py-1 font-medium">Job Search</a>
